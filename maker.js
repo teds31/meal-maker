e1ab6b66ef87b6e13b874c3db34b132b7458906c
Teddy Stanowski <tstanowski31@gmail.com>
2017-10-09 17:05:44 -0700

    add getters and setters

---
+++ maker.js
@@ -0,0 +1,34 @@
+let menu = {
+  _courses: {
+  	appetizers: [],
+    mains: [],
+    desserts: []
+  },
+  get courses() {
+    return {
+    	appetizers: this._courses.appetizers,
+    	mains: this._courses.mains,
+      desserts: this._courses.desserts
+    };
+  },
+
+  get appetizers() {
+
+  },
+  set appetizers(appertizerIn) {
+
+  },
+
+  get mains() {
+
+  },
+  set mains(mainIn) {
+
+  },
+
+ 	get desserts() {
+
+  },
+  set desserts(dessertIn) {
+
+  },
