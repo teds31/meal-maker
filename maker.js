e9811cde7236acf32af344df3eedb26c0da2c6ed
Teddy Stanowski <tstanowski31@gmail.com>
2017-10-09 17:09:17 -0700

    add methods to generate dishes

--- maker.js
+++ maker.js
@@ -33,4 +33,26 @@ let menu = {
 
   },
 
+  addDishToCourse(courseName, dishName, dishPrice) {
+    let dish = {
+      name: dishName,
+      price: dishPrice
+    }
+  },
+
+  getRandomDishFromCourse(courseName) {
+    let dishes = this._courses[courseName];
+    let randomIndex = Math.floor(Math.random() * dishes.length);
+  },
+
+  generateRandomMeal(){
+    let appetizer = this.getRandomDishFromCourse('appetizers');
+    let mains = this.getRandomDishFromCourse('mains');
+    let desserts = this.getRandomDishFromCourse('desserts');
+    let totalPrice = appetizer.price + main.price + dessert.price;
+
+    return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name}. The price is $${totalPrice}.`;
+
+  }
+
 };
